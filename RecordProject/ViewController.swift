bca57ff53d2c3a39e52c9f78ddd3489cf50bd735
yosei-yamagishi <yamagishi@radiotalk.jp>
2020-08-29 12:34:04 +0900

    Initial Commit

---
+++ RecordProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RecordProject
+//
+//  Created by Yosei Yamagishi on 2020/08/29.
+//  Copyright © 2020 Yosei Yamagishi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
